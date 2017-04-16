68a3a56de3e1565fffa03e01d3654e5346e74f2b
hekai <hekai@qq.com>
2017-04-16 22:51:00 +0800

    堙

--- TestGit/TestGit/Program.cs
+++ TestGit/TestGit/Program.cs
@@ -11,6 +11,24 @@ namespace TestGit
         static void Main(string[] args)
         {
             var cc = "宋青青傻B";
+
+            cc = "宋青青是个大傻B";
+
+            Program pm = new Program();
+            var tm = pm.GetUser();
+        }
+
+        /// <summary>
+        /// 获取用户
+        /// </summary>
+        /// <returns></returns>
+        public TestModult GetUser()
+        {
+            TestModult tm = new TestModult();
+            tm.UserName = "hekai";
+            tm.Address = "湖北蕲春";
+            var fs = "";
+            return tm;
         }
     }
 }
